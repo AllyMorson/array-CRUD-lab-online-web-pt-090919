a8a0e6b002670237c0c3515079d4771f99998fee
Ally Morson <ally.morson@gmail.com>
2019-09-20 16:57:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  empty_array = []
+[]
 end
 
 def create_an_array
-  pets = ["dog", "cat", "bird", "mouse"]
+["dog", "cat", "bird", "mouse"]
 end
 
 def add_element_to_end_of_array(array, element)
